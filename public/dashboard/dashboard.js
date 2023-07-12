f21e47fc09631cb4b1e2fec21b25104b1e4d944a
huichiaotsou <huichiao.tsou@gmail.com>
2023-07-12 22:13:19 +0800

    user hamburger menu added

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -3,17 +3,3 @@ function loadUser() {
   const welcomeText = document.getElementById("welcome_text");
   welcomeText.textContent = `Welcome Home! ${user.user_define_name}`;
 }
-
-function addAdminButton() {
-  const user = JSON.parse(localStorage.getItem("user"));
-  if (!user.is_admin) {
-    return;
-  }
-
-  const divElement = document.createElement("div");
-  divElement.setAttribute("class", "button");
-  divElement.setAttribute("id", "admin_button");
-  divElement.textContent = "管理員專區";
-  divElement.setAttribute("onclick", "redirect('/admin.html')");
-  document.body.appendChild(divElement);
-}
--- public/utils.js
+++ public/utils.js
@@ -1,3 +1,32 @@
 function redirect(path) {
   window.location.href = path;
 }
+
+function addAdminButton() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user.is_admin) {
+    return;
+  }
+
+  const divElement = document.createElement("div");
+  divElement.setAttribute("class", "button");
+  divElement.setAttribute("id", "admin_button");
+  divElement.textContent = "管理介面";
+  divElement.setAttribute("onclick", "redirect('/admin.html')");
+  document.body.appendChild(divElement);
+}
+
+function addAdminMenuOptions() {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user.is_admin) {
+    return;
+  }
+
+  const menuList = document.getElementById("menu_list");
+
+  const adminDashboardLi = document.createElement("li");
+  adminDashboardLi.setAttribute("onclick", "redirect('/admin.html')");
+  // adminDashboardLi.onclick = ;
+  adminDashboardLi.textContent = "管理介面";
+  menuList.appendChild(adminDashboardLi);
+}
